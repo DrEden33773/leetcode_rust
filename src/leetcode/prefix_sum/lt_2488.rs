46c8c10b5232e25310be3874eb812e2b8d997560
DrEden33773 <edwardwang33773@gmail.com>
2023-03-16 08:32:29 +0800

    update

---
+++ src/leetcode/prefix_sum/lt_2488.rs
@@ -0,0 +1,11 @@
+#![allow(dead_code, unused_mut)]
+
+pub struct Solution;
+
+impl Solution {
+    pub fn count_subarrays(nums: Vec<i32>, k: i32) -> i32 {
+        let mut res = 0;
+
+        res
+    }
+}
