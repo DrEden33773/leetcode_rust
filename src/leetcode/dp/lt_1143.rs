af52c04b5a6ad82e3c35057ca1d8f566b2b06521
DrEden33773 <edwardwang33773@gmail.com>
2023-03-29 12:10:10 +0800

    update

--- src/leetcode/dp/lt_1143.rs
+++ src/leetcode/dp/lt_1143.rs
@@ -3,12 +3,12 @@
 pub struct Solution;
 
 impl Solution {
-    pub fn longest_common_subsequence(text1: String, text2: String) -> i32 {
+    pub fn better_lcs(text1: String, text2: String) -> i32 {
         let (m, n) = (text1.len(), text2.len());
         let (mut prev, mut curr) = (vec![0; n + 1], vec![0; n + 1]);
         for i in 1..=m {
             for j in 1..=n {
-                curr[j] = if text1.chars().nth(i - 1) == text2.chars().nth(j - 1) {
+                curr[j] = if text1.as_bytes()[i - 1] == text2.as_bytes()[j - 1] {
                     prev[j - 1] + 1
                 } else {
                     std::cmp::max(prev[j], curr[j - 1])
@@ -17,5 +17,20 @@ impl Solution {
             (prev, curr) = (curr, prev)
         }
         prev[n]
+    }
+
+    pub fn longest_common_subsequence(text1: String, text2: String) -> i32 {
+        let (m, n) = (text1.len(), text2.len());
+        let mut dp = vec![vec![0; n + 1]; m + 1];
+        for i in 1..=m {
+            for j in 1..=n {
+                dp[i][j] = if text1.as_bytes()[i - 1] == text2.as_bytes()[j - 1] {
+                    dp[i - 1][j - 1] + 1
+                } else {
+                    std::cmp::max(dp[i - 1][j], dp[i][j - 1])
+                }
+            }
+        }
+        dp[m][n]
     }
 }
