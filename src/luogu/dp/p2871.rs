071ac26f3a8f80dee7d7568b79ec9615eeb403bb
DrEden33773 <edwardwang33773@gmail.com>
2023-04-08 19:17:51 +0800

    update

--- src/luogu/dp/p2871.rs
+++ src/luogu/dp/p2871.rs
@@ -1,20 +1,17 @@
 #![allow(dead_code)]
 
-use std::io::{self, Read};
+use std::io;
 
 #[inline]
 fn backpack_01_solution(weights: &[usize], values: &[usize], capacity: usize) -> usize {
     let num = weights.len();
-    let mut dp = vec![0; num + 1];
+    let mut dp = vec![0; capacity + 1];
     // i => item_upper_lim
     for i in 1..=num {
+        let (weight, value) = (weights[i - 1], values[i - 1]);
         // j => sub_capacity
-        for j in (1..=capacity).rev() {
-            dp[j] = dp[j];
-            let (weight, value) = (weights[i - 1], values[i - 1]);
-            if weight <= j {
-                dp[j] = dp[j].max(dp[j - weight] + value)
-            }
+        for j in (weight..=capacity).rev() {
+            dp[j] = dp[j].max(dp[j - weight] + value)
         }
     }
     dp[capacity]
@@ -22,19 +19,41 @@ fn backpack_01_solution(weights: &[usize], values: &[usize], capacity: usize) ->
 
 #[inline]
 pub fn exec_interface() {
+    /* buffer */
+    let mut buf: Vec<&str>;
+    /* var */
+    let mut num = 0usize;
+    let mut capacity = 0usize;
+    let mut weights = Vec::<usize>::new();
+    let mut values = Vec::<usize>::new();
+    /* read proc */
     let mut str = String::new();
-    let mut buf = vec![];
-    let (mut num, mut capacity) = (0usize, 0usize);
-    let (mut weights, mut values) = (Vec::<usize>::new(), Vec::<usize>::new());
     io::stdin().read_line(&mut str).unwrap();
-    buf = str.split(' ').collect();
-    (num, capacity) = (buf[0].parse().unwrap(), buf[1].parse().unwrap());
+    buf = str.trim().split(' ').collect();
+    num = buf[0].parse().unwrap();
+    capacity = buf[1].parse().unwrap();
     for _ in 0..num {
+        let mut str = String::new();
         io::stdin().read_line(&mut str).unwrap();
-        buf = str.split(' ').collect();
+        buf = str.trim().split(' ').collect();
         weights.push(buf[0].parse().unwrap());
         values.push(buf[1].parse().unwrap());
     }
+    /* res */
     let res = backpack_01_solution(&weights, &values, capacity);
     println!("{res}");
+}
+
+#[cfg(test)]
+mod p2871 {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let weights = vec![1, 2, 3, 2];
+        let values = vec![4, 6, 12, 7];
+        let capacity = 6;
+        let res = backpack_01_solution(&weights, &values, capacity);
+        eprintln!("{res}")
+    }
 }
