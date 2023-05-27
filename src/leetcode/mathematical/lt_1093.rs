05b89a0cfec0089004f042753c0246fc8dc3e775
DrEden33773 <edwardwang33773@gmail.com>
2023-05-27 17:31:45 +0800

    update

---
+++ src/leetcode/mathematical/lt_1093.rs
@@ -0,0 +1,9 @@
+#![allow(dead_code)]
+
+pub struct Solution;
+
+impl Solution {
+    pub fn sample_stats(count: Vec<i32>) -> Vec<f64> {
+        unimplemented!()
+    }
+}
--- src/leetcode/mathematical/mod.rs
+++ src/leetcode/mathematical/mod.rs
@@ -1,3 +1,4 @@
+pub mod lt_1093;
 pub mod lt_2413;
 pub mod lt_2409;
 pub mod lt_1017;
