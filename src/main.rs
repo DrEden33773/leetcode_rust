5ff1466c0f7380c3fc93cf29263f335889abdbd0
DrEden33773 <edwardwang33773@gmail.com>
2023-05-27 19:05:51 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,18 @@ mod issue;
 mod leetcode;
 mod luogu;
 
-fn main() {
-    println!();
+fn i64_to_f64_test() {
+    println!("{}", i64::MAX);
+    println!("{}", f64::MAX_EXP);
+    println!()
+}
+
+fn show_info() {
     println!("Please use `test` instead of running in `main func`...");
     println!()
+}
+
+fn main() {
+    show_info();
+    i64_to_f64_test();
 }
