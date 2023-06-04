859fa39f170430b8c23c4274792c4edc252ab39a
DrEden33773 <edwardwang33773@gmail.com>
2023-06-04 20:44:34 +0800

    update

---
+++ src/leetcode/hash_table/lt_2465.rs
@@ -0,0 +1,20 @@
+#![allow(dead_code)]
+
+pub struct Solution;
+
+use std::collections::HashSet;
+
+impl Solution {
+    pub fn distinct_averages(mut nums: Vec<i32>) -> i32 {
+        let mut set = HashSet::new();
+        nums.sort_unstable();
+        let mut left = 0;
+        let mut right = nums.len() - 1;
+        while left < right {
+            set.insert(nums[left] + nums[right]);
+            left += 1;
+            right -= 1;
+        }
+        set.len() as i32
+    }
+}
