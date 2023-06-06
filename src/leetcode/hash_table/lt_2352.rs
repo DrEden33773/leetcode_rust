5da7a3da83c1cffc8cec1627e0b0857f31141e7c
DrEden33773 <edwardwang33773@gmail.com>
2023-06-07 00:09:37 +0800

    update

---
+++ src/leetcode/hash_table/lt_2352.rs
@@ -0,0 +1,22 @@
+#![allow(dead_code)]
+
+pub struct Solution;
+
+use std::ops::AddAssign;
+
+impl Solution {
+    pub fn equal_pairs(grid: Vec<Vec<i32>>) -> i32 {
+        let mut cnt = grid
+            .iter()
+            .fold(std::collections::HashMap::new(), |mut cnt, row| {
+                cnt.entry(row.to_owned()).or_insert(0).add_assign(1);
+                cnt
+            });
+        (0..grid.len()).fold(0, |ans, col| {
+            let curr_col = (0..grid.len())
+                .map(|row| grid[row][col])
+                .collect::<Vec<_>>();
+            ans + *cnt.entry(curr_col).or_insert(0)
+        })
+    }
+}
