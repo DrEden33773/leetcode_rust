bff71535012f0896af495fa7ef64f0fdaffec9f1
DrEden33773 <edwardwang33773@gmail.com>
2023-06-14 20:43:15 +0800

    update

--- src/leetcode/hash_table/lt_532.rs
+++ src/leetcode/hash_table/lt_532.rs
@@ -1,7 +1,19 @@
 crate::Solution!();
 
+use std::collections::HashSet;
+
 impl Solution {
     pub fn find_pairs(nums: Vec<i32>, k: i32) -> i32 {
-        unimplemented!()
+        let mut appeared = HashSet::new();
+        let mut diffs = HashSet::new();
+        nums.iter().for_each(|&num| {
+            [num - k, num + k].iter().for_each(|tar| {
+                if appeared.contains(tar) {
+                    diffs.insert((num.min(*tar), num.max(*tar)));
+                }
+            });
+            appeared.insert(num);
+        });
+        diffs.len() as i32
     }
 }
