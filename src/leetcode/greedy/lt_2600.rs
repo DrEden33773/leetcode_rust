da7408cb6c88695c6554565a23b00a29d1eb2b28
DrEden33773 <edwardwang33773@gmail.com>
2023-07-06 05:22:33 +0800

    update

--- src/leetcode/greedy/lt_2600.rs
+++ src/leetcode/greedy/lt_2600.rs
@@ -4,7 +4,7 @@ impl Solution {
     pub fn k_items_with_maximum_sum(
         num_ones: i32,
         num_zeros: i32,
-        num_neg_ones: i32,
+        _num_neg_ones: i32,
         k: i32,
     ) -> i32 {
         match k {
