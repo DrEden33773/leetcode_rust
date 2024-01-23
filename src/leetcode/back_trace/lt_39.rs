bb30c7ca8e8debad7e9156a8eaeaecc238660599
DrEden33773 <edwardwang33773@gmail.com>
2024-01-23 14:20:42 +0800

    update

--- src/leetcode/back_trace/lt_39.rs
+++ src/leetcode/back_trace/lt_39.rs
@@ -2,6 +2,7 @@ crate::sln!();
 
 impl Solution {
   pub fn combination_sum(candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
+    let _ = target;
     let mut candidates = candidates;
     candidates.sort();
     unimplemented!()
